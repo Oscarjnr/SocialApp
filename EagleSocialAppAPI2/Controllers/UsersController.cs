5491ae933bc607b48303a9997d6dd0f9885935f2
Yinka Adeyemi <yinkaadeyemi121@gmail.com>
2024-09-23 19:24:03 +0100

    API Walking Skeleton

---
+++ EagleSocialAppAPI2/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using EagleSocialAppAPI2.Data;
+using EagleSocialAppAPI2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EagleSocialAppAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(AppDbContext dbContext) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var appUsers = await dbContext.AppUsers.ToListAsync();
+            return appUsers;
+        }
+
+        [HttpGet("{id:int}")] //api/users/1
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var appUser = await dbContext.AppUsers.FindAsync(id);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return appUser;
+        }
+
+    }
+}
---
+++ EagleSocialAppAPI2/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using EagleSocialAppAPI2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EagleSocialAppAPI2.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
